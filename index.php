74cc695e616b63123dd4da5671be66fd0b9fde65
grozny1999 <59829932+grozny1999@users.noreply.github.com>
2021-12-06 18:09:34 +0100

    Ny funksjon
    Ny funksjoner

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
-<<<<<<< Updated upstream
-<?php  
- $connect = mysqli_connect("localhost", "root", "", "testing");  
- session_start();  
- if(isset($_SESSION["username"]))  
- {  
-      header("location:home.php");  
- }  
- if(isset($_POST["login"]))  
- {  
-      if(empty($_POST["username"]) && empty($_POST["password"]))  
-      {  
-           echo '<script>alert("Begge felt er nødvendig")</script>';  
-      }  
-      else  
-      {  
-           $username = mysqli_real_escape_string($connect, $_POST["username"]);  
-           $password = mysqli_real_escape_string($connect, $_POST["password"]);  
-           $password = md5($password);  
-           $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
-           $result = mysqli_query($connect, $query);  
-           if(mysqli_num_rows($result) > 0)  
-           {  
-                $_SESSION['username'] = $username;  
-                header("location:home.php");  
-           }  
-           else  dsfds
-           {  
-                echo '<script>alert("Feil brukernavn eller passord, vennligst prøv igjen")</script>';  
-           }  
-      }  
- }  
- ?>  
-=======
 <?php   
 $connect = mysqli_connect("localhost", "root", "", "test");  
 session_start();  
@@ -64,7 +30,6 @@ if(isset($_POST["login"]))
      }  
 }  
 ?>
->>>>>>> Stashed changes
  <!DOCTYPE html>  
  <html>  
       <head>
