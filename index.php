08a5c8d75b2087103361c4afe8948f538e8978d1
grozny1999 <59829932+grozny1999@users.noreply.github.com>
2021-12-08 15:37:48 +0100

    Final
    update

--- index.php
+++ index.php
@@ -1,43 +1,11 @@
-<?php   
-$connect = mysqli_connect("localhost", "root", "", "test");  
-session_start();  
-if(isset($_SESSION["username"]))  
-{  
-     header("location:home.php");  
-}  
-if(isset($_POST["login"]))  
-{  
-     if(empty($_POST["username"]) && empty($_POST["password"]))  
-     {  
-          echo '<script>alert("Begge felt er nødvendig")</script>';  
-     }  
-     else  
-     {  
-          $username = mysqli_real_escape_string($connect, $_POST["username"]);  
-          $password = mysqli_real_escape_string($connect, $_POST["password"]);  
-          $password = md5($password);  
-          $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
-          $result = mysqli_query($connect, $query);  
-          if(mysqli_num_rows($result) > 0)  
-          {  
-               $_SESSION['username'] = $username;  
-               header("location:home.php");  
-          }  
-          else  
-          {  
-               echo '<script>alert("Feil brukernavn eller passord, vennligst prøv igjen")</script>';  
-          }  
-     }  
-}  
-?>
- <!DOCTYPE html>  
- <html>  
-      <head>
-      <link rel="icon" href="images\icon.png">  
-           <title>Innloggingsportal</title>  
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+<?php session_start(); ?>
+<!DOCTYPE html>  
+<html>  
+      <head>  
+     <title>Neo ungdomssklubb</title>  
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
       </head>  
       <body style="background-color:#F7FFFF;">>  
            <br /><br />  
